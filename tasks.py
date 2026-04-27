e4718e063dbe5da2fead782ff82bb6a417275708
Georgiy Kiselev <gsx2002@yandex.ru>
2026-04-28 01:19:34 +0300

    tasks classes fixed

--- tasks.py
+++ tasks.py
@@ -9,18 +9,22 @@ from scipy.stats import pearsonr
 
 from pathlib import Path
 import os
+from catboost import CatBoostRegressor
+from shapely import wkt
 
 
 class BaseTask(ABC):
     def __init__(
         self,
-        dataset_link: str,
         dataset_path: str,
         features: list[str],
         target_col: str,
         geom_col: str,
         val_ratio: float,
         model: Any,
+        dataset_link: str | None = None,
+        dataset_crs: str = 'EPSG:4326',
+        cat_features: list[str] = [],
     ):
         super().__init__()
         self.dataset_link = dataset_link
@@ -36,21 +40,23 @@ class BaseTask(ABC):
         self.y_val: gpd.pd.Series = None
         self.x_test: gpd.GeoDataFrame = None
         self.y_test: gpd.pd.Series = None
-        
+
         self.x_train_geom: gpd.GeoSeries = None
         self.x_val_geom: gpd.GeoSeries = None
         self.x_test_geom: gpd.GeoSeries = None
-        
+
+        self.dataset_crs = dataset_crs
         self.crs = 'EPSG:4326'
+        self.cat_features = cat_features
 
     @abstractmethod
     def _load_dataset(self) -> tuple[Any, Any]:
         pass
 
-    def _drop_geom_cols(self) -> None:
-        self.x_train = self.x_train.drop(columns=[self.geom_col])
-        self.x_val = self.x_val.drop(columns=[self.geom_col])
-        self.x_test = self.x_test.drop(columns=[self.geom_col])       
+    def _drop_cols(self, columns: list[str]) -> None:
+        self.x_train = self.x_train.drop(columns=columns)
+        self.x_val = self.x_val.drop(columns=columns)
+        self.x_test = self.x_test.drop(columns=columns)       
 
     def prepare_dataset(self) -> None:
         X, y = self._load_dataset()
@@ -60,27 +66,27 @@ class BaseTask(ABC):
         self.x_val, self.x_test, self.y_val, self.y_test = train_test_split(
             self.x_val, self.y_val, test_size=0.5, random_state=42
         )
-        
+
         self.x_train_geom = self.x_train[self.geom_col]
         self.x_val_geom = self.x_val[self.geom_col]
         self.x_test_geom = self.x_test[self.geom_col]
-    
-        self._drop_geom_cols()
+
+        self._drop_cols([self.geom_col, self.target_col])
 
     def add_embeddings(
         self,
         embeddings: gpd.GeoDataFrame,
         emb_geom_col: str
     ) -> None:
-        
+
         self.x_train[self.geom_col] = self.x_train_geom
         self.x_val[self.geom_col] = self.x_val_geom
         self.x_test[self.geom_col] = self.x_test_geom
-        
+
         self.x_train = gpd.GeoDataFrame(self.x_train).set_geometry(self.geom_col).set_crs(self.crs)
         self.x_val = gpd.GeoDataFrame(self.x_val).set_geometry(self.geom_col).set_crs(self.crs)
         self.x_test = gpd.GeoDataFrame(self.x_test).set_geometry(self.geom_col).set_crs(self.crs)
-        
+
         self.x_train = self.x_train.sjoin(embeddings)
         self.x_val = self.x_val.sjoin(embeddings)
         self.x_test = self.x_test.sjoin(embeddings)
@@ -88,8 +94,8 @@ class BaseTask(ABC):
         emb_col_set = set(embeddings.columns)
         emb_col_set.remove(emb_geom_col)
         self.features.extend(list(emb_col_set))
-        
-        self._drop_geom_cols()    
+
+        self._drop_cols([self.geom_col])    
 
     def train_and_eval_model(self, param_distributions: dict, n_trials: int = 100) -> dict:
         def objective(trial):
@@ -107,7 +113,11 @@ class BaseTask(ABC):
                     raise ValueError(f"Unsupported parameter range format for {param_name}")
 
             model_instance = self.model.__class__(**{**self.model.get_params(), **params})
-            model_instance.fit(self.x_train, self.y_train)
+            model_instance.fit(
+                self.x_train,
+                self.y_train,
+                cat_features=self.cat_features
+            )
 
             y_pred_val = model_instance.predict(self.x_val)
             val_rmse = root_mean_squared_error(self.y_val, y_pred_val, squared=False)
@@ -118,7 +128,11 @@ class BaseTask(ABC):
 
         best_params = study.best_params
         best_model = self.model.__class__(**{**self.model.get_params(), **best_params})
-        best_model.fit(self.x_train, self.y_train)
+        best_model.fit(
+            self.x_train,
+            self.y_train,
+            cat_features=self.cat_features
+        )
 
         y_pred_test = best_model.predict(self.x_test)
 
@@ -139,7 +153,14 @@ class BaseTask(ABC):
 class FlatsTask(BaseTask):
     def _load_dataset(self):
         dataset = gpd.read_file(self.dataset_path)
-        X = dataset[self.features]
+        if isinstance(dataset[self.geom_col].iloc[0], str):
+            dataset[self.geom_col] = dataset[self.geom_col].apply(wkt.loads)
+
+        dataset = gpd.GeoDataFrame(dataset).set_geometry(self.geom_col)
+        dataset[self.geom_col] = dataset[self.geom_col]\
+            .set_crs(self.dataset_crs).to_crs(self.crs)
+
+        X = dataset[self.features + [self.geom_col]]
         y = dataset[self.target_col]
 
         return X, y
@@ -153,10 +174,9 @@ class FNSTask(BaseTask):
         os.chdir(os.path.join(root_dir, 'parsers', 'fns_parser'))
         from parsers import start
 
-        save_path = os.path.join(root_dir, 'datasets', 'fns')
-        save_path_level_10 = os.path.join(save_path, 'fns_level_10.csv')
-        save_path_level_7 = os.path.join(save_path, 'fns_level_7.csv')
-        os.makedirs(save_path, exist_ok=True)
+        save_path_level_10 = os.path.join(self.dataset_path, 'fns_level_10.csv')
+        save_path_level_7 = os.path.join(self.dataset_path, 'fns_level_7.csv')
+        os.makedirs(self.dataset_path, exist_ok=True)
 
         if not os.path.exists(save_path_level_10):
             start(save_path_level_10, resolution=10)
@@ -164,3 +184,51 @@ class FNSTask(BaseTask):
             start(save_path_level_7, resolution=10)
 
         os.chdir(cwd)
+
+
+if __name__ == '__main__':
+    from pathlib import Path
+    root_dir = Path(__file__).resolve().parent
+
+    flats_dataset_path = os.path.join(root_dir, 'datasets', 'flats', 'flats_merged.csv')
+
+    model = CatBoostRegressor()
+    flats_task = FlatsTask(
+        dataset_path=flats_dataset_path,
+        features=[
+            'dealType', 'roomsCount', 'repairType', 'hasFurniture', 'isApartments',
+            'floorNumber', 'flatType', 'livingArea', 'windowsViewType', 'balconiesCount',
+            'kitchenArea', 'isRecidivist', 'totalArea', 'hasLift', 'buildYear',
+            'materialType' ,'distance_to_center', 'highways_count', 'undergrounds_count',
+            'railways_count', 'time_to_metro', 'price'
+        ],
+        geom_col='geometry',
+        val_ratio=0.2,
+        model=model,
+        target_col='price',
+        dataset_crs='EPSG:3857',
+        cat_features=[
+            'hasFurniture', 'isApartments', 'flatType',
+            'isRecidivist', 'hasLift', 'materialType',
+            'windowsViewType'
+        ]
+    )
+
+    flats_task.prepare_dataset()
+
+    fns_dataset_path = os.path.join(root_dir, 'datasets', 'fns')
+
+    fns_task = FNSTask(
+        dataset_path=fns_dataset_path,
+        features=[
+            'CacheBillPercent', 'CachePayPercent', 'IntensityOfNumberBills', 'RevenueIntensity',
+            'PeriodStart', 'PeriodEnd', 'IsMall', 'IsRare', 'IsEcommerce', 'TopCategories',
+            'Title', 'ActiveHours', 'ReceiptTotalCount'
+        ],
+        geom_col='coordinates',
+        val_ratio=0.2,
+        model=model,
+        target_col='KktCount'
+    )
+
+    fns_task.prepare_dataset()
